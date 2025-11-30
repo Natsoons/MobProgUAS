1df8a74d6fdc3338130d0fb9d51f9da38e768a2f
Backburner <martinferdinandharno@gmail.com>
2025-11-30 18:20:25 +0700

    fix user model

--- lib/model/users.dart
+++ lib/model/users.dart
@@ -1,5 +1,15 @@
 class User {
   final String name;
   final String email;
-  User({required this.name, required this.email});
+  final String? profileImage;
+
+  User({required this.name, required this.email, this.profileImage});
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      name: map['name'] ?? '',
+      email: map['email'] ?? '',
+      profileImage: map['profile_image'],
+    );
+  }
 }
